f1dc156fa245dc222d532d4af8f6c553e8f125e0
Alex Perkowski <perkowskialex@gmail.com>
2019-10-25 16:52:36 -0500

    event listener for clicking, turn count functional, able to make x and o

--- js/main.js
+++ js/main.js
@@ -9,50 +9,84 @@ const winPattern = [
     [4,5,6],
     [7,8,9]
 ];
-const xBoxes = [];
-const oBoxes = [];
-const xWinCheck = 3; 
-const oWinCheck = 3;
-const turn = 1; 
+let xWinCheck = 0; 
+let oWinCheck = 0;
 const gameDrawMove = 10; //when turn hits this number, declare draw
+let turn = 1; 
+
 
 /*----- app's state (variables) -----*/
 
 /*----- cached element references -----*/
+const message = document.getElementById('message');
 
 /*----- event listeners -----*/
-function render() {
-//check win
-    document.getElementById('board').addEventListener('click', (event) => {
-        let message = document.getElementById('message');
-        let t = event.target;
-        if (turn >= gameDrawMove) {
-            //Game over, you drew
-            message.textContent = 'Draw! Play again?';
+// document.querySelector('board')
+//   .addEventListener('click', handleClick);
+let board1 = document.querySelectorAll('.cell')
+    // .addEventListener('click');
+    board1.forEach(element => {
+        // console.log(element)
+        element.addEventListener('click', handleClick);
+    });
+    // console.log();
+// replayBtn.addEventListener('click', init); EVENTUALLY WE WILL ADD THIS
+
+
+/*----- functions -----*/   
+function handleClick(event) {
+        turn++;
+        console.log(turn);
+        if (turn > gameDrawMove) {
+            message.textContent = "Draw! Want to play again?";
+            return;
         }
-        else if (turn % 2  ===   0) {
+        let t = event.target;
+        // console.log(t.tagName); // ==> DIV
+        if (turn % 2  ===   0) {
             message.textContent = "Player O's turn";
             t.innerHTML = "X";
+            t.style.backgroundColor='red';
+            xWinCheck++;
+            console.log("xWinCheck = "+xWinCheck);
+            // xBoxes.push
         }
-        else if (turn % 2 === 1) {
+        if (turn % 2 === 1) {
             message.textContent = "Player X's turn";
             t.innerHTML = "O";
+            t.style.backgroundColor='yellow';
+            oWinCheck++;
+            console.log("oWinCheck = "+oWinCheck);
         }
-        turn++; //add to turn so eventlistener works
-    });
-    //if xWinCheck or oWinCheck > 3    
-        //for winPattern[i] 
-            //if xBoxes = winPattern[i] 
-                //x win
-            // if oBoxes = winPattern[i]
-                //o win
-} //end of checkWin();
+    }
 
-/*----- functions -----*/         
 function init() {
-    
+    let xBoxes = [];
+    let oBoxes = [];
+    render(); 
 } //end of init function
 
+function render() {
+    if (turn >= gameDrawMove) {
+        //Game over, you drew
+        message.textContent = 'Draw! Play again?';
+    }
+    if (xWinCheck > 3 || oWinCheck > 3) {
+    winPattern.forEach(element, idx, function() {
+        if (element[idx].includes(xBoxes)) {
+            message.textContent = 'Player X won!'
+        }
+        if (element[idx].includes(oBoxes)) {
+            message.textContent = 'Player O won!'
+        }
+        else {
+            return;
+        }
+        });
+    }
+
+}
+
 //call functions
-init();
-render();
+// init();
+// render();
