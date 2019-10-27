5d419c05a1fe6b95a066b893d9b2b75b8a85c78b
Alex Perkowski <perkowskialex@gmail.com>
2019-10-27 15:10:25 -0500

    set up loop to display winner, still not working

--- js/main.js
+++ js/main.js
@@ -1,96 +1,73 @@
 /*----- constants -----*/
 const winPattern = [
-    [1,2,3],
-    [1,5,9],
-    [1,4,7],
-    [2,5,8],
-    [3,5,7],
-    [3,6,9],
-    [4,5,6],
-    [7,8,9]
+    [1, 2, 3],
+    [1, 5, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 5, 7],
+    [3, 6, 9],
+    [4, 5, 6],
+    [7, 8, 9]
 ];
-let xWinCheck = 0; 
-let oWinCheck = 0;
 const gameDrawMove = 10; //when turn hits this number, declare draw
-let turn = 1; 
+let turn = 1;
 let xBoxes = [];
-let oBoxes =[];
+let oBoxes = [];
 
 
 /*----- app's state (variables) -----*/
 
 /*----- cached element references -----*/
 const message = document.getElementById('message');
+const winner = document.getElementById('winner');
 
 /*----- event listeners -----*/
 // document.querySelector('board')
 //   .addEventListener('click', handleClick);
 let board1 = document.querySelectorAll('.cell')
-    // .addEventListener('click');
-    board1.forEach(element => {
-        // console.log(element)
-        element.addEventListener('click', handleClick);
-    });
-    // console.log();
+// .addEventListener('click');
+board1.forEach(element => {
+    // console.log(element)
+    element.addEventListener('click', handleClick);
+});
+// console.log();
 // replayBtn.addEventListener('click', init); EVENTUALLY WE WILL ADD THIS
 
 
-/*----- functions -----*/   
+/*----- functions -----*/
 function handleClick(event) {
-        turn++;
-        console.log(turn);
-        if (turn > gameDrawMove) {
-            message.textContent = "Draw! Want to play again?";
-            return;
-        }
-        let t = event.target;
-        console.log("Cell ID: "+t.id); // ==> cell number
-        if (turn % 2  ===   0) {
-            message.textContent = "Player O's turn";
-            t.innerHTML = "X";
-            t.style.backgroundColor='red';
-            xWinCheck++;
-            console.log("xWinCheck = "+xWinCheck);
-            xBoxes.push(t.id);
-            console.log("xBoxes: " + xBoxes);
-        }
-        if (turn % 2 === 1) {
-            message.textContent = "Player X's turn";
-            t.innerHTML = "O";
-            t.style.backgroundColor='yellow';
-            oWinCheck++;
-            console.log("oWinCheck = "+oWinCheck);
-            oBoxes.push(t.id);
-            console.log("oBoxes: " + oBoxes);
-        }
-        render();
-    }
-
-function init() {
-    render(); 
-} //end of init function
-
-function render() {
+    turn++;
+    console.log(turn);
     if (turn >= gameDrawMove) {
-        //Game over, you drew
-        message.textContent = 'Draw! Play again?';
+        message.textContent = "Draw! Want to play again?";
+        return;
     }
-    if (xWinCheck >= 3 || oWinCheck >= 3) {
-    winPattern.forEach(e, function() {
-        if (e.includes(xBoxes)) {
-            message.textContent = 'Player X won!'
-        }
-        if (e.includes(oBoxes)) {
-            message.textContent = 'Player O won!'
-        }
-        else {
-            return;
-        }
-        });
+    let t = event.target;
+    console.log("Cell ID: " + t.id); // ==> cell number
+    if (turn % 2 === 0) {
+        message.textContent = "Player O's turn";
+        t.innerHTML = "X";
+        t.style.backgroundColor = 'red';
+        xBoxes.push(t.id);
+        console.log("xBoxes: " + xBoxes);
     }
-
+    if (turn % 2 === 1) {
+        message.textContent = "Player X's turn";
+        t.innerHTML = "O";
+        t.style.backgroundColor = 'yellow';
+        oBoxes.push(t.id);
+        console.log("oBoxes: " + oBoxes);
+    }
+    for (let i = 0; i < winPattern.length; i++) {
+        if (xBoxes.includes(winPattern[i][0]) && xBoxes.includes(winPattern[i][1]) && xBoxes.includes(winPattern[i][2])) {
+            winner.textContent = 'Player X wins';
+            console.log(winner);
+        }
+        if (oBoxes.includes(winPattern[i][0]) && oBoxes.includes(winPattern[i][1]) && oBoxes.includes(winPattern[i][2])) {
+            winner.textContent = 'Player O wins';
+            console.log(winner);
+        }
+    }
 }
-
 //call functions
 // init();
-// render();
