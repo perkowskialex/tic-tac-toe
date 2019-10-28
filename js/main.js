cf7353f9eed6fc9262c58b764ff60c5a7edca541
Alex Perkowski <perkowskialex@gmail.com>
2019-10-28 13:54:01 -0500

    reset button working - reloads page

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const winPattern = [
     [4, 5, 6],
     [7, 8, 9]
 ];
-const gameDrawMove = 11; //when turn hits this number, declare draw
+const gameDrawMove = 10; //when turn hits this number, declare draw
 
 /*----- app's state (variables) -----*/
 let turn = 1;
@@ -17,7 +17,7 @@ let xBoxes = [];
 let oBoxes = [];
 let winPatternStr = [];
 let xWin = false;
-let yWin = false;
+let oWin = false;
 
 /*----- cached element references -----*/
 const message = document.getElementById('message');
@@ -35,9 +35,13 @@ resetBtn.addEventListener('click',reset);
 
 /*----- functions -----*/
 function handleClick(event) {
+    if (reset) {
+       let turn = 1;
+       let reset = false; 
+    }
     turn++;
-    let t = event.target; 
-    if (yWin || xWin) {
+    let t = event.target;
+    if (oWin || xWin) {
         console.log("No click - game is over");
         return; 
     }
@@ -47,17 +51,17 @@ function handleClick(event) {
         message.textContent = "Draw! Want to play again?";
         return;
     }
-    if ((turn % 2 === 0) && t.innerHTML!=='X' &&t.innerHTML!=='O') {
+    if ((turn % 2 === 0) && t.innerHTML!=='X' && t.innerHTML!=='O') {
         message.textContent = "O's turn";
         t.innerHTML = "X";
-        t.style.backgroundColor = 'red';
+        t.style.backgroundColor = 'lightslategrey';
         xBoxes.push(t.id);
         console.log("xBoxes: " + xBoxes);
     }
-    if ((turn % 2 === 1)  && t.innerHTML!=='X' &&t.innerHTML!=='O') {
+    if ((turn % 2 === 1)  && t.innerHTML!=='X' && t.innerHTML!=='O') {
         message.textContent = "X's turn";
         t.innerHTML = "O";
-        t.style.backgroundColor = 'yellow';
+        t.style.backgroundColor = 'lightslategrey';
         oBoxes.push(t.id);
         console.log("oBoxes: " + oBoxes);
     }
@@ -81,7 +85,6 @@ function checkWin() {
     oBoxes.sort(function(a,b) {return a-b});
     let oStrings = oBoxes.join('');
     console.log("oStrings: "+oStrings);
-    console.log("o:"+oBoxes);
     winPatternStr.forEach(function(element) {
         if (xStrings.includes(element)) {
             message.textContent = 'X WINS!';
@@ -90,27 +93,14 @@ function checkWin() {
         }
         else if (oStrings.includes(element)) {
             message.textContent = 'O WINS';
-            yWin = true;
+            oWin = true;
             return;
         }
     });
 }
 
 function reset() {
-    console.log('Reset starting');
-    let turn = 1;
-    let xBoxes = [];
-    let oBoxes = [];
-    let winPatternStr = [];
-    let xWin = false;
-    let yWin = false;
-    let message = "X's turn - click to start";
-    board1.forEach(function(element) {
-        element.style.backgroundColor = 'white';
-        element.innerHTML = '';
-        element.addEventListener('click', handleClick);
-    });
-    console.log('Reset complete');
+    location.reload();
 } 
 
 //call functions?
